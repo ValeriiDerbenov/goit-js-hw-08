70bb78aeb039fb58883b0c3476a12258243e48ed
Valerii <vd.alternative@gmail.com>
2023-11-01 12:30:13 +0200

    12345

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,5 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
