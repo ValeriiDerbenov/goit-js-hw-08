257e476ed437a289d25a9544eea90e5e58c3dfcc
Valerii <vd.alternative@gmail.com>
2023-11-01 13:33:16 +0200

    css1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,3 +3,11 @@ import throttle from 'lodash.throttle';
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveState = () => {
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(state));
+};
