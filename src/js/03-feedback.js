73885c069e7ee0b1209f0a2e8af209e281d6c6b3
Valerii <vd.alternative@gmail.com>
2023-11-03 09:57:21 +0200

    03-02

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,4 +12,31 @@ const saveState = () => {
   localStorage.setItem('feedback-form-state', JSON.stringify(state));
 };
 
-console.log(state);
+const updateFormState = () => {
+  const state = JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+  emailInput.value = state.email || '';
+  messageInput.value = state.message || '';
+};
+
+const submitForm = event => {
+  event.preventDefault();
+  const state = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+
+  if (emailInput.value === '' || messageInput.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  console.log(state);
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+emailInput.addEventListener('input', throttle(saveState, 500));
+messageInput.addEventListener('input', throttle(saveState, 500));
+form.addEventListener('submit', submitForm);
+
+updateFormState();
